a8beae624562425eeb655a77d071e205772c81ea
unknown <VishwajeetPatil0101>
2024-04-19 17:01:59 +0530

    check code is added??

--- script1.sh
+++ script1.sh
@@ -1,8 +1,10 @@
 echo "We are trying this practical from linux EC2"
-echo "We are from batch-24 and learning git/gihub"
+echo "We are from batch-24 and learning git/gihub"ls
 echo "Happy weekend"
 echo "we are from batch 23 "
 echo "we are from batch 23"
 
 
 echo "vishwajeet patil " 
+echo " cheacking code is commiting or not"
+
