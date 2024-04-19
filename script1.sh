1bd0d0929850ad9feef4e7b2dca321d889f09962
unknown <VishwajeetPatil0101>
2024-04-19 16:59:31 +0530

    this is my first commite

--- script1.sh
+++ script1.sh
@@ -2,3 +2,7 @@ echo "We are trying this practical from linux EC2"
 echo "We are from batch-24 and learning git/gihub"
 echo "Happy weekend"
 echo "we are from batch 23 "
+echo "we are from batch 23"
+
+
+echo "vishwajeet patil " 
